9c0432496597a1eeb78fe32c7fe3b41f143f122f
Anthony Peruso <perusoanthony@gmail.com>
2021-11-19 16:40:23 -0500

    Add comments and some more cleanup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     list2: []
   });
 
-  // Get saved lists localStorage item and set the state
+  // Get saved lists localStorage item and set the state on page load
   useEffect(() => {
     const savedLists = localStorage.getItem('lists');
     if (savedLists) {
@@ -22,10 +22,12 @@ export default function App() {
     localStorage.setItem('lists', JSON.stringify(lists));
   }, [lists]);
 
+  // Simple clone function so we aren't messing with the mutability of objs we're working with
   const cloneDeep = (obj) => {
     return JSON.parse(JSON.stringify(obj));
   }
 
+  // Take in the new list item, deep clone the existing state, push the new item onto the first list by default
   const onListFormSubmit = (newListItem) => {
     setLists(prevLists => {
       const newLists = cloneDeep(prevLists);
@@ -34,6 +36,7 @@ export default function App() {
     });
   }
 
+  // Deep clone the previous state, return a filtered array of list items for the list that this item was a part of
   const onListItemDelete = (listKey, listItemId) => {
     setLists(prevLists => {
       const newLists = cloneDeep(prevLists)
@@ -42,6 +45,8 @@ export default function App() {
     });
   }
 
+  // Deep clone the previous state, find the list item in the array it currently belongs to,
+  // filter the existing list array to remove this list item, and then depending on which list the item came from, push it onto the opposite array
   const onListItemMove = (listKey, listItemId) => {
     setLists(prevLists => {
       const newLists = cloneDeep(prevLists);
@@ -56,6 +61,7 @@ export default function App() {
     });
   }
 
+  // Simple counter for how many items we have total living in both lists
   const totalListItems = lists.list1.length + lists.list2.length;
 
   return (
--- src/ListGroup.js
+++ src/ListGroup.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ListItem from './ListItem';
 
 export default function ListGroup({ listKey, listName, listItems, onListItemDelete, onListItemMove }) {
-  // console.log(listItems);
   return (
     <div className="w-100 lg:w-1/2 lg:max-w-sm mb-20 lg:mb-0">
       <h3 className="text-lg font-bold mb-3">{listName}</h3>
+      {listItems.length === 0 && <p class="text-sm text-gray-500">No items have been added to this list, move items into here from the other list, or start adding them by using the form above.</p>}
       {listItems.map(listItem => {
         return <ListItem
           key={listItem.id}
